bdda7da79453d85586bffdd08460e6ecdd1288d7
Nahuelitor <dospaparulos@gmail.com>
2023-05-03 16:06:20 -0300

    validación form por JS terminado.

--- js/formulario.js
+++ js/formulario.js
@@ -5,11 +5,22 @@ const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	dni: /^\d{1,14}$/, // 0 a 14 numeros.
-    fecNac:/^[0-9\_\-]{4,16}$/,
+    fecNac:/^[0-9\_\-]{10}$/,
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-    dia:/ /,
-    hora:/ /,
+	telefono: /^[\d\+]{1,16}$/, // 1 a 16 numeros y puede estar el + por código de país.
+    dia:/^[0-9\_\-]{10}$/,
+    hora:/^[0-9\:\-]{5}$/,
+}
+
+const campos = {
+    nombre: false,
+    apellido: false,
+    dni: false,
+    fecNac: false,
+    correo: false,
+    telefono: false,
+    dia: false,
+    hora: false,
 }
 
 const validarFormulario = (e) => {
@@ -27,16 +38,16 @@ const validarFormulario = (e) => {
             validarCampo(expresiones.fecNac, e.target, 'fecNac');
         break;
         case "correo":
-            console.log('Funciona!')
+            validarCampo(expresiones.correo, e.target, 'correo');
         break;
         case "telefono":
-            console.log('Funciona!')
+            validarCampo(expresiones.telefono, e.target, 'telefono');
         break;
         case "dia":
-            console.log('Funciona!')
+            validarCampo(expresiones.dia, e.target, 'dia');
         break;
         case "hora":
-            console.log('Funciona!')
+            validarCampo(expresiones.hora, e.target, 'hora');
         break;
     }
 }
@@ -48,12 +59,14 @@ const validarCampo = (expresion, input, campo) => {
         document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
         document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
         document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos[campo] = true;
     } else {
         document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
         document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
         document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
         document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
         document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos[campo] = false;
     }
 }
 
@@ -62,6 +75,25 @@ inputs.forEach((input) => {
     input.addEventListener('blur', validarFormulario )
 })
 
-formulario.addEventListener('submit', ()  => {
+formulario.addEventListener('submit', (e)  => {
     e.preventDefault();
+
+    if(campos.nombre && campos.apellido && campos.dni && campos.fecNac && campos.correo && campos.telefono && campos.dia && campos.hora){
+        formulario.reset();
+
+        document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+        document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 10000);
+
+
+        document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+            icono.classList.remove('formulario__grupo-correcto');
+        });
+    
+    } else {
+        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+    
+    }
 })
